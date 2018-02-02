9844bbb61508d00c0f07213e1f8c2176b6d356c8
CDharmateja <dharmateja03@yahoo.com>
2018-02-02 19:40:26 +0530

    added question

--- homework/PA_1/counting_inversions.py
+++ homework/PA_1/counting_inversions.py
@@ -1,3 +1,14 @@
+# Programming Question 1:
+
+# The IntegerArray.txt file contains all of the 100,000 integers between 1 and 100,000 (inclusive) in some order, with no integer repeated.
+# Your task is to compute the number of inversions in the file given, where the ith row of the file indicates the ith entry of an array.
+# Because of the large size of this array, you should implement the fast divide-and-conquer algorithm covered in the video lectures.
+# The numeric answer for the given input file should be typed in the space below.
+# So if your answer is 1198233847, then just type 1198233847 in the space provided without any spaces or commas or any other punctuation marks. You can make up to 5 attempts.
+
+
+# Solution:
+
 def merge_sort(L):
     if len(L) < 2:
         return L[:], 0
